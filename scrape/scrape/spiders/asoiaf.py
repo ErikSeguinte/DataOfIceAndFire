fd6f18e5e14efe5be73bdb7120a9e0f0bdcf7d9b
Erik Seguinte <ErikSeguinte@gmail.com>
2020-02-26 20:43:42 -0800

    deletes chapter grid crawl

--- scrape/scrape/spiders/asoiaf.py
+++ scrape/scrape/spiders/asoiaf.py
@@ -6,23 +6,12 @@ class BookSpider(scrapy.Spider):
 
     def parse(self, response):
         # Follow links to books
-        for title in response.css('h3 a'):
-            pass
-            yield response.follow(title, self.parse_title)
-
         for title in response.css('.postmeta li:nth-child(3) a'):
             yield response.follow(title, self.parse_chapters)
-
-    def parse_title(self, response):
-        yield {
-            'response' : response,
-            'title': response.css('h2::text').get(),
-            'chapters':response.css('.binding .chaptergrid a::text').getall()}
 
     def parse_chapters(self, response):
         print(response.css("h3 a::text").getall())
         yield {
-            'response' : response,
             'chapters' : response.css("h3 a::text").getall(),
             'scores' : response.css('.rightend a::text').getall()
         }
