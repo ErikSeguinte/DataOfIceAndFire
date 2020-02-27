6ad634cdd3d2dc09fa3454022d0db6c569ac81c0
Erik Seguinte <ErikSeguinte@gmail.com>
2020-02-26 20:33:18 -0800

    update to scraping

--- scrape/scrape/spiders/asoiaf.py
+++ scrape/scrape/spiders/asoiaf.py
@@ -5,7 +5,24 @@ class BookSpider(scrapy.Spider):
     start_urls = ['https://towerofthehand.com/books/guide.html']
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        # Follow links to books
+        for title in response.css('h3 a'):
+            pass
+            yield response.follow(title, self.parse_title)
+
+        for title in response.css('.postmeta li:nth-child(3) a'):
+            yield response.follow(title, self.parse_chapters)
+
+    def parse_title(self, response):
+        yield {
+            'response' : response,
+            'title': response.css('h2::text').get(),
+            'chapters':response.css('.binding .chaptergrid a::text').getall()}
+
+    def parse_chapters(self, response):
+        print(response.css("h3 a::text").getall())
+        yield {
+            'response' : response,
+            'chapters' : response.css("h3 a::text").getall(),
+            'scores' : response.css('.rightend a::text').getall()
+        }
